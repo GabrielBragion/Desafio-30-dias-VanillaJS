b5fce02c46addefa0b75b5afe0c96a3a185e7326
Gabriel Bragion <contato@gabrielbragion.com.br>
2022-07-04 11:26:54 +0100

    Add files via upload

---
+++ Dia 11 - Custom Video Player/index.js
@@ -0,0 +1,67 @@
+/* Get ELEMENTS */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const controls = document.querySelector(".player__controls")
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+/* Build FUNCTIONS */
+function togglePlay(){
+  if(video.paused){
+    video.play()
+  }else{
+    video.pause()
+  }
+}
+
+function updateButton(){
+  const icon = this.paused ? '►' : '❚ ❚'
+  toggle.textContent = icon
+  if(this.paused){
+    controls.classList.add("active")
+  }else{
+    controls.classList.remove("active")
+  }
+}
+
+function skip(){
+  const skipValue = this.dataset.skip
+  video.currentTime += parseFloat(skipValue)
+}
+
+function handleRangeUpdate(){
+  video[this.name] = this.value
+}
+
+function handleProgress(){
+  const percent = (video.currentTime / video.duration) * 100
+  progressBar.style.flexBasis = `${percent}%`
+}
+
+function scrub(e){
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration
+  video.currentTime = scrubTime
+}
+
+/* EVENT LISTNERS */
+video.addEventListener("click", togglePlay)
+video.addEventListener("play", updateButton)
+video.addEventListener("pause", updateButton)
+video.addEventListener("timeupdate", handleProgress)
+
+toggle.addEventListener("click", togglePlay)
+
+skipButtons.forEach(btn => btn.addEventListener("click", skip))
+
+ranges.forEach(range => range.addEventListener("change", handleRangeUpdate))
+ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate))
+
+let mouseDown = false
+progress.addEventListener("click", scrub)
+progress.addEventListener("mousemove", (e) => mouseDown && scrub(e))
+progress.addEventListener("mousedown", () => mouseDown = true)
+progress.addEventListener("mouseup", () => mouseDown = false)
